775164c649dd4f76357d7f97315fb9d94cc4f759
Abodkabarity <abod.kabarity@gmail.com>
2026-01-22 15:51:56 +0400

    Update web build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "556108435"
+    serviceWorkerVersion: "3154108260"
   }
 });
