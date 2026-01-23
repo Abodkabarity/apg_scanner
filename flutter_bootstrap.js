e453aaa367ed32dcb10f3754ca8d0a9790d33697
Abodkabarity <Abodkabarity@users.noreply.github.com>
2026-01-23 12:37:42 +0000

    deploy: 3a58a2ce21b93d13ed1d57366ccd5d5de16b1c42

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2052465508"
+    serviceWorkerVersion: "1051405358"
   }
 });
